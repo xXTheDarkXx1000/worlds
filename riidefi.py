cb87a6a4e218eea71e35db75cbcc831e090ee557
xXTheDarkXx1000 <44132849+xXTheDarkXx1000@users.noreply.github.com>
2018-10-18 23:31:02 +0200

    Update riidefi.py

--- riidefi.py
+++ riidefi.py
@@ -13,11 +13,11 @@ Clientdiscord = discord.Client()
 
 
 @client.event
-    if message.content.startswith('!!give-money @riidefi#4002 (\d.*) '):
-        await client.send_message(message.channel,'Hey Thanks for giving me (\d.*) <@%s>!'  %(message.author.id))
-    if message.content.startswith('!!give-money riidefi#4002 (\d.*)'):
-        await client.send_message(message.channel,'Hey Thanks for giving me (\d.*) <@%s>!'  %(message.author.id))
-    if message.content.startswith('!!give-money riidefi (\d.*) '):
-        await client.send_message(message.channel,'Hey Thanks for giving me (\d.*) <@%s>!'  %(message.author.id))
+   if message.content.startswith('!!give-money @riidefi#4002 (\d.*) '):
+       await client.send_message(message.channel,'Hey Thanks for giving me (\d.*) <@%s>!'  %(message.author.id))
+   if message.content.startswith('!!give-money riidefi#4002 (\d.*)'):
+       await client.send_message(message.channel,'Hey Thanks for giving me (\d.*) <@%s>!'  %(message.author.id))
+   if message.content.startswith('!!give-money riidefi (\d.*) '):
+       await client.send_message(message.channel,'Hey Thanks for giving me (\d.*) <@%s>!'  %(message.author.id))
 client.run('NTAxNDUwODAzOTY2Mzc3OTk0.DqZnmA.C0Q-jEFZRLoz0B90g2ZBBQAHa0A')
 
