8b73a5abbb4e0fe9229d7bb68b8b5e51a68bddfa
xXTheDarkXx1000 <44132849+xXTheDarkXx1000@users.noreply.github.com>
2018-10-18 23:25:29 +0200

    Update riidefi.py

--- riidefi.py
+++ riidefi.py
@@ -17,5 +17,5 @@ async def on_message(message):
     source_message = ".."
     searched = re.search(r"!!give.*riidefi.*(\d.*)", source_message)
     if searched and searched.group(0):
-     send_message("Thanks, %s for $%s!" % (sender.name, searched.group(0)))
+        send_message("Thanks, %s for $%s!" % (sender.name, searched.group(0)))
 client.run('NTAxNDUwODAzOTY2Mzc3OTk0.DqZnmA.C0Q-jEFZRLoz0B90g2ZBBQAHa0A')
